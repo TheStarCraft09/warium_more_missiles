23728792fd5e100a9a47975121b20349c9ecfaf8
richard <richard-git@nehmer.net>
2026-08-06 12:31:33 +0200

    [Refactor] Rename ExampleMod to MoreMissiles and update related configurations

--- src/main/java/dev/lumareth/moreMissiles/MoreMissiles.java
+++ src/main/java/dev/lumareth/moreMissiles/MoreMissiles.java
@@ -1,17 +1,6 @@
-package com.yourname.examplemod;
+package dev.lumareth.moreMissiles;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -24,33 +13,22 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MOD_ID)
-public class ExampleMod {
+@Mod(MoreMissiles.MOD_ID)
+public class MoreMissiles {
 
-    // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "examplemod"; // No spaces, no uppercase
-    // Directly reference a slf4j logger
+    public static final String MOD_ID = "more_missiles"; // No spaces, no uppercase
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod() {
+    public MoreMissiles() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
